6d9cf02bb0bdbcd5f1117ebb9e8ec1fb94aee270
Manimaran R <31967065+manimaran6483@users.noreply.github.com>
2020-07-11 17:52:46 +0530

    Update main.js

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,16 +1,14 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
-
-
-//search states.json and filter
+var i;
 const searchStates = async searchText => {
-	const response = await fetch('https://cors-anywhere.herokuapp.com/http://localhost:5000/');
-    const states = await response.json();
-	console.log(states);	
+	const response = await fetch('https://cors-anywhere.herokuapp.com/https://manimaran.pythonanywhere.com/');
+	states = await response.json();
+	console.log(states);
 	//get Matches to current text input
 	let matches = states.filter(state => {
 		const regex = new RegExp(`^${searchText}`, 'gi');
-		return states[0].match(regex);
+		return state.name.match(regex);
 	});
 	if (searchText.length == 0) {
 		matches = [];
@@ -22,14 +20,18 @@ const searchStates = async searchText => {
 //show resulsts
 const outputHtml = matches => {
 	if (matches.length > 0) {
-		const html = matches.map(match => `
-			<div>
-				<h4>${match}</h4>
-			</div>
-		`).join('');
+		const html = matches.map(match =>
+			`<p id="sug" onclick="change(${JSON.stringify(match.name)})">
+			${JSON.stringify(match.name)}
+		</p>`
+		).join('');
+
 		matchList.innerHTML = html;
 	}
 }
-
+//fetchapi();
 search.addEventListener('input', () => searchStates(search.value));
 
+function change(value) {
+	document.getElementById("sug").value = value;
+}
